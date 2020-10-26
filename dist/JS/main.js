6a88979a03a01362d3b1286a1fa8f7622ce97c48
lera lescuyer <lllcommunication@gmx.fr>
2020-10-26 18:20:30 +0100

    fin du projet

---
+++ dist/JS/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".btn").on('mouseenter',function (e) {
+       x = e.pageX - $(this).offset().left;
+       y = e.pageY - $(this).offset().top;
+
+        $(this).find('span').css({top:y,left:x});
+    });
+        $(".btn").on('mouseout',function (e) {
+       x = e.pageX - $(this).offset().left;
+       y = e.pageY - $(this).offset().top;
+
+        $(this).find('span').css({top:y,left:x});
+    });
+})
